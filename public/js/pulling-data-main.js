0fdd91029696890e42afd03562fd8b76babcc8b7
Evan Smith <evansmith@Evans-MacBook-Pro.local>
2017-12-18 21:09:23 -0700

    one more for the books

---
+++ public/js/pulling-data-main.js
@@ -0,0 +1,154 @@
+var mainVue = new Vue({
+    el: '#app',
+    data: {
+        // arr of strings that are coin abbreviations, used this array to build arrOfCoins
+        arrOfCoinAbbreviations: [],
+
+        // array of coin objects with id and other properties
+        arrOfCoins: [],
+
+        // array of coin objects that have social info
+        arrOfCoins2: []
+    },
+    methods: {
+        pullData: function(event){
+            event.preventDefault();
+
+            $.get('/pullData', function(dataObj){
+
+                var coinObjs = dataObj.Data;
+                console.log(coinObjs);
+
+
+                //Add data objects to array
+                for(var coin in coinObjs){
+                    // each coin variable is just a string with a few letters and numbers, ex. Bitcoin is 'BTC'
+                    // make an array of strings
+                    mainVue.arrOfCoinAbbreviations.push(coin);
+                }
+
+                // make a new array of objects with id and coinName property
+                for(var i = 0; i < mainVue.arrOfCoinAbbreviations.length; i++){
+
+                    var coinAbbrev = mainVue.arrOfCoinAbbreviations[i];
+                    var coinId = coinObjs[mainVue.arrOfCoinAbbreviations[i]].Id;
+                    var coinName = coinObjs[mainVue.arrOfCoinAbbreviations[i]].CoinName;
+                    var coinFullName = coinObjs[mainVue.arrOfCoinAbbreviations[i]].FullName;
+                    var coinTotalSupply = coinObjs[mainVue.arrOfCoinAbbreviations[i]].TotalCoinSupply;
+
+                    var coinOb = {
+                        Abbrev: coinAbbrev,
+                        Id: coinId,
+                        Name: coinName,
+                        FullName: coinFullName,
+                        TotalCoinSupply: coinTotalSupply
+
+                    }
+                    mainVue.arrOfCoins.push(coinOb);
+                }
+                console.log(typeof mainVue.arrOfCoins[1].TotalCoinSupply);
+
+            }) // End of get request
+
+        }, // End of pullData
+
+
+        saveData: function(event){
+            event.preventDefault();
+
+            // Sending an object with one property, an array of static coin objects
+            var objectToSend = {
+                arrOfCoinObjs : mainVue.arrOfCoins
+            }
+
+            $.post('/saveData', objectToSend, function(dataFromServer){
+                console.log(dataFromServer);
+            })
+
+        },
+
+        pullWantedData: function(event) {
+            event.preventDefault();
+
+            console.log(mainVue.arrOfCoins.length);
+
+            for(var i = 0; i < mainVue.arrOfCoins.length; i++){
+
+                var idObjToSend = {
+                    id: mainVue.arrOfCoins[i].Id
+                }
+
+                $.post('/pullWantedData', idObjToSend, function(dataFromServer){
+
+                    var hasList = dataFromServer.Data.CodeRepository.List[0];
+
+                    var abbrev1 = dataFromServer.Data.General.Name;
+                    var name1 =  dataFromServer.Data.General.CoinName;
+
+                    var repoUrl1 = '';
+                    var repoStars1 = '';
+                    var repoForks1 = '';
+                    var repoSubscribers1 = '';
+
+                    if(typeof hasList === 'undefined'){
+                        repoUrl1 = '';
+                        repoStars1 = '';
+                        repoForks1 = '';
+                        repoSubscribers1 = '';
+                    } else {
+                        
+                        repoUrl1 = dataFromServer.Data.CodeRepository.List[0].url;
+                        repoStars1 = dataFromServer.Data.CodeRepository.List[0].stars;
+                        repoForks1 = dataFromServer.Data.CodeRepository.List[0].forks;
+                        repoSubscribers1 = dataFromServer.Data.CodeRepository.List[0].subscribers;
+                    }
+
+                    var coin = {
+                        abbrev: abbrev1,
+                        name: name1,
+                        repoUrl: repoUrl1,
+                        repoStars : repoStars1,
+                        repoForks : repoForks1,
+                        repoSubscribers : repoSubscribers1
+                    }
+
+                    mainVue.arrOfCoins2.push(coin);
+
+                }) // end post
+
+            } // end for loops
+
+        },
+        saveWantedData: function(event){
+            event.preventDefault();
+
+            // Sending an object with one property, an array of wanted coin objects
+            var objectToSend = {
+                arrOfCoinObjs : mainVue.arrOfCoins2
+            }
+
+            $.post('/saveWantedData', objectToSend, function(dataFromServer){
+                console.log(dataFromServer);
+            })
+
+        },
+        consoleToCheck: function(event){
+            event.preventDefault();
+            console.log(mainVue.arrOfCoins2);
+        }
+
+        // saveDataForAll : function(event){
+        //     event.preventDefault();
+
+        //     var infoToSend = {
+        //         staticCoinObjs: mainVue.arrOfCoins
+        //     }
+
+        //     $.post('/saveDataForAll', infoToSend, function(dataFromServer){
+        //         console.log(dataFromServer);
+        //     })
+        // }
+        
+    } // end methods
+
+}) // end mainVue
--- pulling-data-server.js
+++ pulling-data-server.js
@@ -7,47 +7,251 @@ var request = require('request');
 var mongoose = require('mongoose');
 const b64 = require('node-b64');
 
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({limit: '50mb', parameterLimit: 1000000, extended:true}));
+app.use(bodyParser.json({limit: '50mb', parameterLimit: 1000000}));
 
 app.use(express.static('./public'));
 
-// mongoose.connect('mongodb://localhost/players', {useMongoClient: true});
+mongoose.connect('mongodb://localhost/coinsInfo', {useMongoClient: true});
 
-// mongoose.connection.once('open', function(){
-//     console.log("Connnection has been made!");
-//     // done();
-// }).on('error', function(error){
-//     console.log("Connection error ", error);
-// });
+mongoose.connection.once('open', function(){
+    console.log("Connnection has been made!");
+    // done();
+}).on('error', function(error){
+    console.log("Connection error ", error);
+});
 
-// new schema for model for the collection 'Players' (array) of player objects in database
+// new schema for model for the collection 'staticcoins' (array) of player objects in database
 
-// var PlayerSchema = new mongoose.Schema({
+var CoinSchema = new mongoose.Schema({
 
-//     team: {
-//         type: String
+    abbrev: {
+        type: String,
+        required: true
 
-//     },
-//     position: {
-//         type: String
-//     },
-//     name: {
-//         type: String
-//     }
+    },
+    _id: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
 
-// });
+    },
+    fullName: {
+        type: String,
+        required: true
+    },
+    totalCoinSupply: {
+        type: String,
+        required: true
+    }
 
-// var PlayerModel = mongoose.model('Player', PlayerSchema);
+});
+
+var CoinModel = mongoose.model('StaticCoin', CoinSchema);
+
+// Schema and Model for 'coins' collection
+
+var Coin2Schema = new mongoose.Schema({
+
+    abbrev: {
+        type: String,
+        required: true
+
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    repoUrl: {
+        type: String,
+        required: true
+
+    },
+    repoStars: {
+        type: String,
+        required: true
+    },
+    repoForks: {
+        type: String,
+        required: true
+    },
+    repoSubscribers: {
+        type: String,
+        required: true
+    }
+
+});
+
+var Coin2Model = mongoose.model('Coin', Coin2Schema);
 
 app.get('/',function(req,res){
-    res.sendFile('./public/html/nflData-index.html',{root: './'});
+    res.sendFile('./public/html/pulling-data-index.html',{root: './'});
 })
 
+app.get('/pullData', function(req,res){
+
+    var url1 = 'https://www.cryptocompare.com/api/data/coinlist/';
+
+    var options1 = {
+        url: url1
+    }
+    request(options1, function(error,response,body){
+
+        //Error check
+        if(error || response.statusCode !== 200){
+            console.log("Failed to send request");
+            res.send("Failed to send request");
+        } else {
+
+        var dataObj1 = JSON.parse(body);
+
+        res.send(dataObj1);
+
+        }
+
+    }) // end request
+
+}) // end app.get '/pullData'
+
+app.post('/saveData', function(req,res){
+
+
+    var coinObjs = req.body.arrOfCoinObjs;
+
+    for(var i = 0; i < coinObjs.length; i++){
+
+        var coinObjToSave = {
+
+            abbrev: coinObjs[i].Abbrev,
+            _id: coinObjs[i].Id,
+            name: coinObjs[i].Name,
+            fullName: coinObjs[i].FullName,
+            totalCoinSupply: coinObjs[i].TotalCoinSupply
+        }
+
+        var newCoin = new CoinModel(coinObjToSave);
+        // save coin to the database
+        newCoin.save(function(err){
+            if(err){
+                res.status(500).send(err);
+            } else {
+                console.log("Saved the coin");
+            }
+        });
+    }
+
+    res.send("Completed");
+
+}) // end app.post for /saveData
+
+app.post('/pullWantedData', function(req,res){
+
+    // req.body is a string id
+    var idToUse = req.body.id;
+
+    var url1 = `https://www.cryptocompare.com/api/data/socialstats/?id=${idToUse}`;
+
+    var options1 = {
+        url: url1
+    }
+
+    request(options1, function(error,response,body){
+
+        //Error check
+        if(error || response.statusCode !== 200){
+            console.log("Failed to send request");
+            res.send("Failed to send request");
+        } else {
+
+        var dataObj1 = JSON.parse(body);
+
+        // sending obj of social statsfor one coin
+        res.send(dataObj1);
+
+        }
+
+    }) // end of request
+
+}) // end post for /pullWantedData
+
+app.post('/saveWantedData', function(req,res){
+
+    var coinObjs = req.body.arrOfCoinObjs;
+    console.log(req.body.arrOfCoinObjs)
+
+    for(var i = 0; i < coinObjs.length; i++){
+
+        var coinObjToSave = coinObjs[i]; 
+
+        // {
+
+        //     abbrev: coinObjs[i].abbrev,
+        //     name: coinObjs[i].name,
+        //     repoUrl: coinObjs[i].repoUrl,
+        //     repoStars: coinObjs[i].repoStars,
+        //     repoForks: coinObjs[i].repoForks,
+        //     repoSubscribers: coinObjs[i].repoSubscribers
+
+        // }
+
+        var newCoin2 = new Coin2Model(coinObjToSave);
+        // save coin to the database
+        newCoin2.save(function(err){
+            if(err){
+                res.status(500).send(err);
+            } else {
+                console.log("Saved the coin");
+            }
+        }); // end save
+
+    } // end for loop
+
+    res.send("Accomplished");
+
+}) // end app.post for /saveWantedData
+
+
+// app.post('/saveDataForAll', function(req,res){
+
+//     var coinObjs = req.body.arrOfCoins;
+
+//     // Now save all coins to new collection in database
+//     for(var i = 0; i < coinsObjs.length; i++){
+
+//         var coinObjToSave = {
+
+//             abbrev: coinObjs[i].Data.General.Name,
+//             name: coinObjs[i].Data.General.CoinName,
+//             repoUrl: coinObjs[i].Data.CodeRepository.List[0].url,
+//             repoStars: coinObjs[i].Data.CodeRepository.List[0].stars,
+//             repoForks: coinObjs[i].Data.CodeRepository.List[0].forks,
+//             repoSubscribers: coinObjs[i].Data.CodeRepository.List[0].subscribers
+//         }
+
+//         var newCoin2 = new Coin2Model(coinObjToSave);
+//         // save coin to the database
+//         newCoin2.save(function(err){
+//             if(err){
+//                 res.status(500).send(err);
+//             } else {
+//                 console.log("Saved the coin");
+//             }
+//         });
+//     } // end of for loop
+
+//     res.send("Done");
+
+// })
+
+/*
 var team;
 var poisition;
 var playerName;
 var baseUrl;
+
 
 app.post('/search',function(req,res){
 
@@ -57,24 +261,6 @@ app.post('/search',function(req,res){
     playerName = req.body.playerName;
 
     baseUrl = `https://api.mysportsfeeds.com/v1.1/pull/nfl/2017-regular/cumulative_player_stats.json?`;
-
-    if(playerName !== ''){
-        baseUrl += `player=${playerName}`;
-    };
-    if((playerName === '') && (teams !== '')){
-        baseUrl += `team=${teams}`;
-    } else if ((playerName !== '') && (teams !== '')){
-        baseUrl += `&team=${teams}`
-    }
-    if((playerName === '') && (teams === '') && (positions !== '')){
-        baseUrl += `position=${positions}`;
-    } else {
-        baseUrl += `&position=${positions}`;
-    } 
-    if((playerName === '') && (teams === '') && (positions === '')){
-        res.send('No fields specified');
-    };
-
 
     'use strict';
     var options = {
@@ -103,6 +289,7 @@ app.post('/search',function(req,res){
     }); // End request from request module
 
 }) //  End app.post
+*/
 
 app.listen(8080, function(){
     console.log('The app is running on port 8080.');
